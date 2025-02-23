1a8d1ff097237175c01fd9e006d55d81f5cad9f3
Punpakorn Pothinalan <p.pothinalan@gmail.com>
2025-02-23 21:13:14 +0700

    Changed few word

--- src/main.rs
+++ src/main.rs
@@ -532,7 +532,7 @@ async fn update_packages() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-/// Display help text.
+/// Display help texts.
 fn print_help() {
     println!("Available commands:");
     println!("  search, s <package>     Search for a package in the AUR and official repositories (sorted by votes).");
